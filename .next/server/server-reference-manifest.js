f84b83a60527a4587d7d29ffe8c074754a278aa1
Puzzo's Codestore <55586438+puzzo-dev@users.noreply.github.com>
2025-04-06 13:54:24 +0000

    Fixes and convert from react to nextjs

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oRsNbfQIRNre4ebAq1eKGVSdaGJBkwbPoY7uwVOdWUM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"o82RVZ6ad7VuNsWTZKCItlvU+k68Symc0x5p5KFtKuc=\"\n}"
